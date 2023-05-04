99f32c6dd740fce1209a836c90a290bb937962e9
Lyuu17 <lyuu17@yandex.com>
2023-05-04 08:56:50 +0200

    Role adding/removing in users service

--- src/properties/properties.controller.ts
+++ src/properties/properties.controller.ts
@@ -103,7 +103,7 @@ export class PropertiesController {
         /* https://stackoverflow.com/a/49096069 */
         // Generate a 32 random chars long string
         const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('')
-        cb(null, `${randomName}${extname(file.originalname)}`)
+        cb(null, `${randomName}${extname(file.originalname)}`);
       }
     }),
     limits: {
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
+import { Role } from 'src/enums/role.enum';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User, UserDocument } from './schemas/user.schema';
 
@@ -27,5 +28,13 @@ export class UsersService {
 
   async count(): Promise<number> {
     return this.userModel.countDocuments().exec();
+  }
+
+  async addRole(id: string, role: Role): Promise<UserDocument> {
+    return this.userModel.findByIdAndUpdate(id, { $addToSet: { roles: role } }, { new: true });
+  }
+
+  async removeRole(id: string, role: Role): Promise<UserDocument> {
+    return this.userModel.findByIdAndUpdate(id, { $pull: { roles: role } }, { new: true });
   }
 }
