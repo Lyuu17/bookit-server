dda479e00942907d788de67f4642aac9ff48963f
Lyuu17 <lyuu17@yandex.com>
2023-06-06 10:01:44 +0200

    Add property/user id missing checks

--- src/itineraries/itineraries.controller.ts
+++ src/itineraries/itineraries.controller.ts
@@ -3,15 +3,19 @@ import { ApiBadRequestResponse, ApiOkResponse } from '@nestjs/swagger';
 import { isMongoId } from 'class-validator';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { Role } from 'src/enums/role.enum';
+import { PropertiesService } from 'src/properties/properties.service';
 import { Roles } from 'src/roles/roles.decorator';
 import { RolesGuard } from 'src/roles/roles.guard';
+import { UsersService } from 'src/users/users.service';
 import { ItineraryDto } from './dto/itinerary.dto';
 import { ItinerariesFacade } from './itineraries.facade';
 
 @Controller('itineraries')
 export class ItinerariesController {
   constructor(
-    private readonly itinerariesFacade: ItinerariesFacade
+    private readonly itinerariesFacade: ItinerariesFacade,
+    private readonly propertiesService: PropertiesService,
+    private readonly usersService: UsersService
   ) { }
 
   @UseGuards(JwtAuthGuard)
@@ -25,10 +29,16 @@ export class ItinerariesController {
   @Roles(Role.ADMIN)
   @Get('user/:q')
   @ApiOkResponse({ description: 'Get all itineraries by user id', type: [ItineraryDto] })
+  @ApiBadRequestResponse({ description: 'Invalid user' })
   @ApiBadRequestResponse()
   async getAllByUserId(@Param('q') q: string) {
     if (!isMongoId(q)) {
       throw new BadRequestException();
+    }
+
+    const user = await this.usersService.findOne(q);
+    if (user == null) {
+      throw new BadRequestException('Invalid user.');
     }
 
     return this.itinerariesFacade.getAllByUserId(q);
@@ -44,6 +54,11 @@ export class ItinerariesController {
       throw new BadRequestException();
     }
 
+    const property = await this.propertiesService.findOne(q);
+    if (property == null) {
+      throw new BadRequestException('Invalid property.');
+    }
+
     return this.itinerariesFacade.getAllByPropertyId(q);
   }
 
@@ -51,6 +66,7 @@ export class ItinerariesController {
   @Post()
   @ApiOkResponse({ description: 'Add an itinerary', type: ItineraryDto })
   @ApiBadRequestResponse({ description: 'Invalid checkin/checkout date' })
+  @ApiBadRequestResponse({ description: 'Invalid property' })
   async addOne(@Body() itineraryDto: ItineraryDto, @Req() req) {
     const dto = new ItineraryDto({ ...itineraryDto,
       user: req.user?.userId
@@ -64,6 +80,11 @@ export class ItinerariesController {
     currentDate.setHours(0, 0, 0);
     if (checkinDate < currentDate || checkoutDate < currentDate) {
       throw new BadRequestException('Invalid checkin/checkout date. Date must be greater than today.');
+    }
+
+    const property = await this.propertiesService.findOne(dto.property);
+    if (property == null) {
+      throw new BadRequestException('Invalid property.');
     }
 
     return this.itinerariesFacade.addOne(dto);
